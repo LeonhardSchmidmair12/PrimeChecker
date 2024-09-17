b6e0fe848c9b0f17c321fd8ff10876c057f63d31
Leonhard Schmidmair <leonhardschmidmair@gmail.com>
2024-09-17 12:18:46 +0200

    Added EratosthenesPrimeSieve class

---
+++ PrimeChecker/src/net/htlgkr/schmidmairL/PrimeSieve.java
@@ -0,0 +1,9 @@
+package net.htlgkr.schmidmairL;
+
+public interface PrimeSieve {
+    public boolean isPrime(int p);
+
+    // Gibt alle Primzahlen bis zur Obergrenze auf der Konsole aus.
+    public void printPrimes();
+
+}
