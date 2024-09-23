ae3772e251564d26f8f68a79a7bf7c1f1649bbef
Leonhard <schmidmairl22102@sus.htl-grieskirchen.at>
2024-09-23 21:51:05 +0200

    EratosthenesPrimeSieve class sieve Method implemented, Main class added- tested- worked

---
+++ PrimeChecker/src/net/htlgkr/schmidmairL/Main.java
@@ -0,0 +1,11 @@
+package net.htlgkr.schmidmairL;
+
+public class Main {
+    public static void main(String[] args) {
+        EratosthenesPrimeSieve eratosthenesPrimeSieve=new EratosthenesPrimeSieve(120);
+
+        eratosthenesPrimeSieve.printPrimes();
+
+        System.out.println(eratosthenesPrimeSieve.isPrime(113));
+    }
+}
