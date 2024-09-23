a07c3ce1ef6b28d4a720ec2f1a42ea79a2c27251
Leonhard <schmidmairl22102@sus.htl-grieskirchen.at>
2024-09-23 21:47:56 +0200

    EratosthenesPrimeSieve class ground functions

--- PrimeChecker/src/net/htlgkr/schmidmairL/EratosthenesPrimeSieve.java
+++ PrimeChecker/src/net/htlgkr/schmidmairL/EratosthenesPrimeSieve.java
@@ -1,7 +1,37 @@
 package net.htlgkr.schmidmairL;
 
-public class EratosthenesPrimeSieve {
+public class EratosthenesPrimeSieve implements PrimeSieve {
+
+    private final int up;
+    private boolean[] gestrichen;
 
 
+    public EratosthenesPrimeSieve(int up) {
+        this.up = up;
+        this.gestrichen = new boolean[up + 1];
+
+    }
+
+
+
+
+
+    @Override
+    public boolean isPrime(int p) {
+        if (p <= 1 || p > up) {
+            return false;
+        }
+        return !gestrichen[p];
+    }
+
+
+    @Override
+    public void printPrimes() {
+        for (int i = 2; i <= up; i++) {
+            if (!gestrichen[i]) {
+                System.out.print(i + ", ");
+            }
+        }
+    }
 
 }
